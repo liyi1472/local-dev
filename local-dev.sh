57694d8984aac1a5799b8f5d01b44334efbcc35d
李毅 <liyi.riki.thomas@gmail.com>
2020-03-24 17:12:15 +0900

    添加数据库配置

--- local-dev.sh
+++ local-dev.sh
@@ -1,8 +1,24 @@
 docker network create \
-  --driver=bridge \
-  --subnet=203.0.0.0/24 \
-  --gateway=203.0.0.1 \
-  local-dev-network
+	--driver=bridge \
+	--subnet=203.0.0.0/24 \
+	--gateway=203.0.0.1 \
+	local-dev-network
+
+docker stop local-dev-mariadb
+docker run -d --rm \
+	--net=local-dev-network \
+	--name=local-dev-mariadb \
+	-v ~/Code/__PROJECT__/local-dev/zoneinfo/Asia/Tokyo:/etc/localtime:ro \
+	-v ~/Code/__PROJECT__/local-dev/mariadb/config:/etc/mysql/conf.d \
+	-e MYSQL_ROOT_PASSWORD=123456 \
+	-e MYSQL_DATABASE=__PROJECT__ \
+	-e MYSQL_USER=admin \
+	-e MYSQL_PASSWORD=123456 \
+	mariadb:latest \
+	--character-set-server=utf8mb4 \
+	--collation-server=utf8mb4_unicode_ci
+
+curl -Lso adminer.php https://www.adminer.org/latest-mysql.php
 
 docker stop local-dev-php
 docker run -d --rm \
